07b3b4042175a910222335278aad05bafbe7569d
LyamDuv <lyam.duval@univ-rouen.fr>
2024-03-15 14:18:20 +0100

    ajout de code

--- script.sh
+++ script.sh
@@ -7,3 +7,4 @@ message="Ceci est un message dans un pop-up."
 # Utiliser Zenity pour afficher le pop-up
 zenity --info --title="$titre" --text="$message" --width=300 --height=150 --timeout=5
 
+echo "je rajoute un echo bonjour" 
