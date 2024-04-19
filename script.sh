d2427b4429203d42453df04fc70b3a3ec96c20fc
Laurent1310 <144345339+Laurent1310@users.noreply.github.com>
2024-04-19 15:11:18 +0200

    Update script.sh hehehe

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 
 #!/bin/bash
-echo "je rajoute un echo bonjour" 
+echo "je rajoute un echo bonjour" >> toto1.txt
 
-echo "test"
+echo "test" >> toto2.txt
 
-echo "ahh"
-echo "yen a mare de laurent"
-echo "Laurent le bosse"
+echo "ahh" >> toto3.txt
+echo "yen a mare de laurent" >> toto4.txt
+echo "Laurent le bosse" >> toto5.txt
